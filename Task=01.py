8c2975352ee83e17fec6f5bab0b740365f1a1707
offical-anurag001 <anuragkumarravi02@gmail.com>
2025-12-07 13:56:21 +0530

    Add files via upload

---
+++ Task=01.py
@@ -0,0 +1,26 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Basic mathematiacl opertion
+>>> 
+>>> num1=int(input("first number :  "))
+first number :  100
+>>> num2=int(input("second number :  "))
+second number :  20
+>>> 
+>>> #Addition
+>>> num1+num2
+120
+>>> #Subtraction
+>>> num1-num2
+80
+>>> #Multiplication
+>>> num1*num2
+2000
+>>> #Division
+>>> num1/num2
+5.0
+>>> 
+>>> #Result
+>>> Addition=120
+>>> subtraction=80
+>>> Multiplication=2000
---
+++ Task=2.py
@@ -0,0 +1,12 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #Assignment
+>>> 
+>>> First_name=str(input("First name : "))
+First name : "Anurag "
+>>> Second_name=str(input("second name : "))
+second name : "Kumar"
+>>> Full_name=str(input("Full name : "))
+Full name : "Anurag kumar "
+>>> print("Hello,"+Full_name+"! walcome!")
+Hello,"Anurag kumar "! walcome!
