1ccb751a3c4f65e7bb90a03b77414d9d6942f7ce
offical-anurag001 <anuragkumarravi02@gmail.com>
2025-12-17 23:28:14 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,42 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+# Task 1: Perform basic mathematical opertion
+
+First_number=int(input("first number : "))
+first number : 100
+
+Second_number=int(input("Second number : "))
+Second number : 20
+
+print(First_number)
+100
+>>> print(Second_number)
+20
+>>> # Addition
+>>> 
+>>> First_number+Second_number
+120
+>>> print(First_number+Second_number)
+120
+>>> # Subtraction
+>>> 
+>>> First_number-Second_number
+80
+>>> print(First_number-Second_number)
+80
+>>> # Multiplication
+>>> First_number*Second_number
+2000
+>>> print(First_number*Second_number)
+2000
+>>> # Division
+>>> First_number/Second_number
+5.0
+>>> print(First_number/Second_number)
+5.0
+>>> # Results
+>>> 
+>>> Addition: 120
+>>> Subtraction: 80
+>>> Multiplication: 2000
+>>> Division: 5
